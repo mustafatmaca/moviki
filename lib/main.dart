176bdf9fa4e89293bc7ed77e603008e5f5bf01a4
mustafatmaca <mustafaatmaca362@gmail.com>
2024-07-25 16:02:33 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Moviki',
+    home: Scaffold(
+      body: Center(
+        child: Text('Welcome Moviki App'),
+      ),
+    ),
+  ));
+}
