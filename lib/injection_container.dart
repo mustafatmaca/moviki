1c015be388913472ff6cf9e45d6c5913bbc46912
mustafatmaca <mustafaatmaca362@gmail.com>
2024-07-31 15:29:01 +0300

    dependency injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:moviki/features/movie/data/data_sources/remote/movie_api_service.dart';
+import 'package:moviki/features/movie/data/repository/movie_repository_impl.dart';
+import 'package:moviki/features/movie/domain/repository/movie_repository.dart';
+import 'package:moviki/features/movie/domain/usecases/get_popular_movie.dart';
+import 'package:moviki/features/movie/presentation/bloc/movie/remote/remote_movie_bloc.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  getIt.registerSingleton<Dio>(Dio());
+
+  getIt.registerSingleton<MovieApiService>(MovieApiService(getIt()));
+
+  getIt.registerSingleton<MovieRepository>(MovieRepositoryImpl(getIt()));
+
+  getIt.registerSingleton<GetPopularMovieUseCase>(
+      GetPopularMovieUseCase(getIt()));
+
+  getIt.registerFactory<RemoteMovieBloc>(() => RemoteMovieBloc(getIt()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moviki/features/movie/presentation/bloc/movie/remote/remote_movie_bloc.dart';
+import 'package:moviki/features/movie/presentation/bloc/movie/remote/remote_movie_event.dart';
 import 'package:moviki/features/splash/presentation/pages/splash_screen.dart';
+import 'package:moviki/injection_container.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Moviki',
-    home: SplashScreen(),
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDependencies();
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<RemoteMovieBloc>(
+        create: (context) => getIt()..add(const GetPopularMovies()),
+      ),
+    ],
+    child: const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Moviki',
+      home: SplashScreen(),
+    ),
   ));
 }
